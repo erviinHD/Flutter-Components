e3b5576563f3c3064b2e2e86833112bcbcedf71a
ervin cabascango <ervin.cabascango.s@gmail.com>
2022-06-04 13:07:05 -0500

    feat: List Separated in Screen

--- lib/screens/listView1_screen.dart
+++ lib/screens/listView1_screen.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 
 class Listview1Screen extends StatelessWidget {
+  final options = const ['Thor', 'Iron Man', 'Hulk', 'Ant Man', 'Spider Man'];
   const Listview1Screen({Key? key}) : super(key: key);
 
   @override
@@ -10,7 +13,14 @@ class Listview1Screen extends StatelessWidget {
           title: const Text('listview Tipo 1'),
         ),
         body: ListView(
-          children: const [Text('heola'), Text('heola'), Text('heola')],
+          children: [
+            ...options
+                .map((heroe) => ListTile(
+                      title: Text(heroe),
+                      trailing: const Icon(Icons.arrow_forward_ios),
+                    ))
+                .toList()
+          ],
         ));
   }
 }
---
+++ lib/screens/listView2_screen.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+  final options = const ['Thor', 'Iron Man', 'Hulk', 'Ant Man', 'Spider Man'];
+  const Listview2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('listview Tipo 2'),
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  title: Text(options[index]),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                ),
+            separatorBuilder: (_, __) => const Divider(),
+            itemCount: options.length));
+  }
+}
