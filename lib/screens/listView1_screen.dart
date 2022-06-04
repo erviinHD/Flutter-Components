88028a59f4eef1fd0c5209051af3de75bc2dc32b
ervin cabascango <ervin.cabascango.s@gmail.com>
2022-06-04 10:41:38 -0500

    first commit

---
+++ lib/screens/listView1_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  const Listview1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('listview Tipo 1'),
+        ),
+        body: ListView(
+          children: const [Text('heola'), Text('heola'), Text('heola')],
+        ));
+  }
+}
