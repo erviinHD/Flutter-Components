6efeb111555b1b7ceedc2f88b88ca15e5fb21afc
ervin cabascango <ervin.cabascango.s@gmail.com>
2022-06-10 00:39:30 -0500

    feat: infinite screoll and pull refresh

--- lib/screens/listview_builder.screen.dart
+++ lib/screens/listview_builder.screen.dart
@@ -1,27 +1,117 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
-class ListBuilderScreen extends StatelessWidget {
+class ListBuilderScreen extends StatefulWidget {
   const ListBuilderScreen({Key? key}) : super(key: key);
 
   @override
+  State<ListBuilderScreen> createState() => _ListBuilderScreenState();
+}
+
+class _ListBuilderScreenState extends State<ListBuilderScreen> {
+  final List<int> imagesId = [1, 2, 3, 4, 5];
+  final ScrollController scrollController = ScrollController();
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if ((scrollController.position.pixels + 500) >=
+          scrollController.position.maxScrollExtent) add5();
+    });
+  }
+
+  void add5() {
+    final lastId = imagesId.last;
+    imagesId.addAll([1, 2, 3, 4, 5].map((e) => lastId + e));
+    setState(() {});
+  }
+
+  Future fetchData() async {
+    if (isLoading) return;
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 3));
+    add5();
+
+    isLoading = false;
+    setState(() {});
+
+    if ((scrollController.position.pixels + 100) <=
+        scrollController.position.maxScrollExtent) return;
+
+    scrollController.animateTo(scrollController.position.pixels + 120,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.fastOutSlowIn);
+  }
+
+  Future<void> onRefresh() async {
+    await Future.delayed(const Duration(seconds: 2));
+    final lastId = imagesId.last;
+    imagesId.clear();
+    imagesId.add(lastId + 1);
+    add5();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: MediaQuery.removePadding(
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (BuildContext context, int index) {
-            return FadeInImage(
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
-                placeholder: const AssetImage('assets/jar-loading.gif'),
-                image: NetworkImage(
-                    'https://picsum.photos/500/300?random=$index'));
-          },
+        child: Stack(
+          children: [
+            RefreshIndicator(
+              color: AppTheme.primary,
+              onRefresh: () => onRefresh(),
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                controller: scrollController,
+                itemCount: imagesId.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return FadeInImage(
+                      width: double.infinity,
+                      height: 300,
+                      fit: BoxFit.cover,
+                      placeholder: const AssetImage('assets/jar-loading.gif'),
+                      image: NetworkImage(
+                          'https://picsum.photos/500/300?random=${imagesId[index]}'));
+                },
+              ),
+            ),
+            if (isLoading)
+              Positioned(
+                  bottom: 40,
+                  left: size.width * 0.5 - 30,
+                  child: const _loadingIcon())
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _loadingIcon extends StatelessWidget {
+  const _loadingIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.8),
+        shape: BoxShape.circle,
+      ),
+      height: 60,
+      width: 60,
+      child: const CircularProgressIndicator(
+        color: AppTheme.primary,
       ),
     );
   }
